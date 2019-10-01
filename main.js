4bc570350a22cbc64b94f53e5854765d6403cd6e
gemeLn <mtang317@gmail.com>
2019-09-30 23:27:11 -0500

    changes

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function setup() {
     screen_height = 600
     fps = 60
     //Output tick
-    opticks = 30
+    runsim = false
     //Environment constants
-    gravityAccel = 98
-    ground = 500
+    gravityAccel = 980
+    ground = 580
     dt = 1 / fps
     //Centermass
     massCx = 0
@@ -20,55 +20,77 @@ function setup() {
     //Initiate
     background(255, 255, 0)
     frameRate(fps)
-    createCanvas(screen_width, screen_height)
-    var n1 = new Node(240, 300)
-    var n2 = new Node(320, 230)
-    var n3 = new Node(270, 300)
-    massCx /= massT
-    massCy /= massT
+    let canvas = createCanvas(screen_width, screen_height)
+    canvas.mousePressed(handleClick)
 
-    for (let n of nodeList) {
-        moment += n.m * (Math.pow(n.x - massCx, 2) + Math.pow(n.y - massCy, 2))
-    }
-    new Edge(n2, n1)
-    new Edge(n2, n3)
-    new Edge(n1, n3)
 }
-var tick = 0
+
+function start() {
+    runsim = true
+}
 //Update and draw
 function draw() {
-    tick++
     clear()
+    background(225)
     //Ground
     line(0, ground, screen_width, ground)
+    if (runsim) {
+        //Calculate center mass and torque
+        massCx = 0
+        massCy = 0
+        grounded = 0
+        torque = 0
+        //Calculate cetner of mass
+        for (let n of nodeList) {
+            n.centerMass()
+        }
+        massCx /= massT
+        massCy /= massT
+        for (let n of nodeList) {
+            n.calc()
+        }
 
-    //Calculate center mass and torque
+        alpha = torque / moment
+        //Update forces and positions of nodes
+        for (let n of nodeList) {
+            n.update()
+            circle(n.x, n.y, n.d, n.d)
+        }
+        //Draw edges
+        for (let e of edgeList) {
+            line(e.n1.x, e.n1.y, e.n2.x, e.n2.y)
+        }
+        stroke(255, 0, 0)
+        circle(massCx, massCy, 3)
+        stroke(0)
+    } else {
+        for (let n of nodeList) {
+            circle(n.x, n.y, n.d, n.d)
+        }
+        for (let e of edgeList) {
+            line(e.n1.x, e.n1.y, e.n2.x, e.n2.y)
+        }
+        stroke(255, 0, 0)
+        circle(massCx, massCy, 3)
+        stroke(0)
+    }
+}
+
+function handleClick() {
+    new Node(mouseX, mouseY)
+    let l = nodeList.length
+    edgeList.pop()
+    if (l > 1) {
+        new Edge(nodeList[l - 1], nodeList[l - 2])
+    }
+    new Edge(nodeList[0], nodeList[l - 1])
+    moment = 0
     massCx = 0
     massCy = 0
-    grounded = 0
-    torque = 0
     for (let n of nodeList) {
+        moment += n.m * (Math.pow(n.x - massCx, 2) + Math.pow(n.y - massCy, 2))
         n.centerMass()
     }
     massCx /= massT
     massCy /= massT
-    for (let n of nodeList) {
-        n.calc()
-    }
-    if (tick == opticks) {
-        tick = 0
-    }
-    alpha = torque / moment
-    //Update forces and positions of nodes
-    for (let n of nodeList) {
-        n.update()
-        circle(n.x, n.y, n.d, n.d)
-    }
-    //Draw edges
-    for (let e of edgeList) {
-        line(e.n1.x, e.n1.y, e.n2.x, e.n2.y)
-    }
-    stroke(255, 0, 0)
-    circle(massCx, massCy, 3)
-    stroke(0)
 }
